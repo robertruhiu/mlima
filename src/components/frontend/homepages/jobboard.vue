09a6d631e8fe2836fe852820427a214cb0d65d82
frenx <robertruhiu@gmail.com>
2019-06-23 15:13:26 +0000

    initial commit base files and components

---
+++ src/components/frontend/homepages/jobboard.vue
@@ -0,0 +1,31 @@
+<template>
+    <a-layout>
+        <Pageheader/>
+        <a-layout-content :style="{ padding: '0 0px', marginTop: '3%' }">
+
+            <div :style="{ background: '#fff', padding: '24px', minHeight: '100vh' }">
+
+            </div>
+        </a-layout-content>
+        <Footer/>
+    </a-layout>
+
+</template>
+
+<script>
+    import Pageheader from '@/components/layout/Header.vue'
+    import Footer from '@/components/layout/Footer.vue'
+    export default {
+        name: "jobboard",
+        components: {
+
+            Pageheader,
+            Footer,
+
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,63 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import {sync} from 'vuex-router-sync'
+import store from '@/store/store'
 
 Vue.config.productionTip = false
+import 'ant-design-vue/dist/antd.css'
+import {
 
+  Alert,
+  Avatar,
+
+  Badge,
+
+  Button,
+
+  Card,
+
+  Col,
+
+
+  Form,
+  Icon,
+  Input,
+  InputNumber,
+  Layout,
+  List,
+  Popover,
+
+  Menu,
+
+
+  Tag,
+
+} from 'ant-design-vue';
+
+
+Vue.use(Alert);
+Vue.use(Avatar);
+Vue.use(Badge);
+Vue.use(Button);
+Vue.use(Card);
+Vue.use(Col);
+Vue.use(Popover)
+Vue.use(Form);
+Vue.use(Icon);
+Vue.use(Input);
+Vue.use(InputNumber);
+Vue.use(Layout);
+Vue.use(List);
+Vue.use(Menu);
+Vue.use(Tag);
+import VueSession from 'vue-session'
+
+Vue.use(VueSession)
+
+sync(store, router);
 new Vue({
+  router,
+  store,
   render: h => h(App),
 }).$mount('#app')
